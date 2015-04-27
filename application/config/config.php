044a4c3f405b9a9ec8406b5cef547caa59ac7f73
mariovalencia <mario.valencia987@gmail.com>
2015-04-27 07:02:03 -0600

    xd

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:8080/CentroServicio';
+$config['base_url']	= 'http://192.168.1.3:8080/CentroServicio';
 
 /*
 |--------------------------------------------------------------------------
